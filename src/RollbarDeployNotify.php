005483a91fc96102b2bd4b7dbe18b4efdce34925
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-06-19 16:39:51 -0700

    spacing, and remove unused comments

--- src/RollbarDeployNotify.php
+++ src/RollbarDeployNotify.php
@@ -21,12 +21,14 @@ class RollbarDeployNotify extends Command {
     protected $description = 'Notify rollbar of a deployment';
     private $accesssToken;
 
-    public function __construct($config) {
+    public function __construct($config)
+    {
         parent::__construct();
         $this->accesssToken = $config['access_token'];
     }
 
-    public function fire() {
+    public function fire()
+    {
         $this->comment("hello");
 
         $environment = env('APP_ENV');
@@ -42,24 +44,19 @@ class RollbarDeployNotify extends Command {
             'access_token' => urlencode($this->accesssToken),
         ];
 
-//url-ify the data for the POST
         foreach ($fields as $key => $value) {
             $fields_string .= $key . '=' . $value . '&';
         }
         rtrim($fields_string, '&');
 
-//open connection
         $ch = curl_init();
 
-//set the url, number of POST vars, POST data
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, count($fields));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 
-//execute post
         $result = curl_exec($ch);
 
-//close connection
         curl_close($ch);
     }
 }
