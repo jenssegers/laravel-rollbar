6076947d40f26051c418e1cbd6d977d57ae8f910
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-06-19 16:37:07 -0700

    undo spacing updates - preserve original spacing

--- src/RollbarServiceProvider.php
+++ src/RollbarServiceProvider.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Jenssegers\Rollbar;
+<?php namespace Jenssegers\Rollbar;
 
 use InvalidArgumentException;
 use RollbarNotifier;
@@ -21,11 +19,13 @@ class RollbarServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function boot() {
+    public function boot()
+    {
         $app = $this->app;
 
         // Listen to log messages.
-        $app['log']->listen(function ($level, $message, $context) use ($app) {
+        $app['log']->listen(function ($level, $message, $context) use ($app)
+        {
             $app['rollbar.handler']->log($level, $message, $context);
         });
     }
@@ -35,13 +35,16 @@ class RollbarServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function register() {
+    public function register()
+    {
         $app = $this->app;
 
-        $this->app['rollbar.client'] = $this->app->share(function ($app) {
+        $this->app['rollbar.client'] = $this->app->share(function ($app)
+        {
             $config = $app['config']->get('services.rollbar');
 
-            if (empty($config['access_token'])) {
+            if (empty($config['access_token']))
+            {
                 throw new InvalidArgumentException('Rollbar access token not configured');
             }
 
@@ -50,7 +53,8 @@ class RollbarServiceProvider extends ServiceProvider {
             return $rollbar;
         });
 
-        $this->app['rollbar.handler'] = $this->app->share(function ($app) {
+        $this->app['rollbar.handler'] = $this->app->share(function ($app)
+        {
             $client = $app['rollbar.client'];
 
             $level = $app['config']->get('services.rollbar.level', 'debug');
@@ -58,33 +62,40 @@ class RollbarServiceProvider extends ServiceProvider {
             return new RollbarLogHandler($client, $app, $level);
         });
         
-        
-        $this->app->singleton('command.rollbar.deploynotify', function($app) {
+        $this->app->singleton('command.rollbar.deploynotify', function($app)
+        {
             $config = $app['config']->get('services.rollbar');
+            
             return new RollbarDeployNotify($config);
         });
         
         $this->commands('command.rollbar.deploynotify');
 
+
         // If the Rollbar client was resolved, then there is a possibility that there
         // are unsent error messages in the internal queue, so let's flush them.
-        register_shutdown_function(function () use ($app) {
-            if (isset($app['rollbar.client'])) {
+        register_shutdown_function(function () use ($app)
+        {
+            if (isset($app['rollbar.client']))
+            {
                 $app['rollbar.client']->flush();
             }
         });
 
         // Register the fatal error handler.
-        register_shutdown_function(function () use ($app) {
-            if (isset($app['rollbar.client'])) {
+        register_shutdown_function(function () use ($app)
+        {
+            if (isset($app['rollbar.client']))
+            {
                 $app->make('rollbar.client');
 
                 Rollbar::report_fatal_error();
             }
         });
     }
-
-    public function provides() {
+    
+    public function provides()
+    {
         return [
             'command.rollbar.deploynotify',
         ];
