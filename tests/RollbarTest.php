jenssegers/laravel-rollbar

commit 8988c224473ed36cf66d3c4a22dd1562c2b33fac

upgrade to use rollbar-php v1.0.0

src/RollbarLogHandler.php
@@ -1,20 +1,20 @@
-<?php namespace Jenssegers\Rollbar;
+<?php namespace Rollbar\Laravel;
 
 use Exception;
 use Illuminate\Foundation\Application;
 use InvalidArgumentException;
 use Monolog\Logger as Monolog;
 use Psr\Log\AbstractLogger;
-use RollbarNotifier;
+use Rollbar\RollbarLogger;
 
 class RollbarLogHandler extends AbstractLogger
 {
     /**
      * The rollbar client instance.
      *
-     * @var RollbarNotifier
+     * @var logger
      */
-    protected $rollbar;
+    protected $logger;
 
     /**
      * The Laravel application.
@@ -50,9 +50,9 @@ class RollbarLogHandler extends AbstractLogger
     /**
      * Constructor.
      */
-    public function __construct(RollbarNotifier $rollbar, Application $app, $level = 'debug')
+    public function __construct(RollbarLogger $logger, Application $app, $level = 'debug')
     {
-        $this->rollbar = $rollbar;
+        $this->logger = $logger;
 
         $this->app = $app;
 
@@ -76,9 +76,9 @@ class RollbarLogHandler extends AbstractLogger
         $context = $this->addContext($context);
 
         if ($message instanceof Exception) {
-            $this->rollbar->report_exception($message, null, $context);
+            return $this->logger->log($level, $message, $context);
         } else {
-            $this->rollbar->report_message($message, $level, $context);
+            return $this->logger->log($level, $message, $context);
         }
     }
 
@@ -91,34 +91,29 @@ class RollbarLogHandler extends AbstractLogger
     {
         // Add session data.
         if ($session = $this->app->session->all()) {
-            if (empty($this->rollbar->person) or ! is_array($this->rollbar->person)) {
-                $this->rollbar->person = [];
-            }
 
             // Merge person context.
             if (isset($context['person']) and is_array($context['person'])) {
-                $this->rollbar->person = $context['person'];
+                
+                $this->logger->configure(['person' => $context['person']]);
                 unset($context['person']);
-            } else {
-                if ($this->rollbar->person_fn && is_callable($this->rollbar->person_fn)) {
-                    $data = @call_user_func($this->rollbar->person_fn);
-                    if (isset($data['id'])) {
-                        $this->rollbar->person = call_user_func($this->rollbar->person_fn);
-                    }
-                }
+                
             }
 
             // Add user session information.
-            if (isset($this->rollbar->person['session'])) {
-                $this->rollbar->person['session'] = array_merge($session, $this->rollbar->person['session']);
-            } else {
-                $this->rollbar->person['session'] = $session;
-            }
+            $config = $this->logger->extend([]);
+            $person = isset($config['person']) ? $config['person'] : [];
+            
+            $person['session'] = isset($person['session']) ? 
+                array_merge($session, $person['session']) :
+                $person['session'] = $session;
 
             // User session id as user id if not set.
-            if (! isset($this->rollbar->person['id'])) {
-                $this->rollbar->person['id'] = $this->app->session->getId();
+            if (! isset($person['id'])) {
+                $person['id'] = $this->app->session->getId();
             }
+                
+            $this->logger->configure(['person' => $person]);
         }
 
         return $context;

tests/RollbarTest.php
@@ -1,6 +1,8 @@
-<?php
+<?php namespace Rollbar\Laravel;
 
-class RollbarTest extends Orchestra\Testbench\TestCase
+use Rollbar\Laravel\Facades\Rollbar as RollbarFacade;
+
+class RollbarTest extends \Orchestra\Testbench\TestCase
 {
     public function setUp()
     {
@@ -12,35 +14,36 @@ class RollbarTest extends Orchestra\Testbench\TestCase
 
     protected function getPackageProviders($app)
     {
-        return ['Jenssegers\Rollbar\RollbarServiceProvider'];
+        return ['Rollbar\Laravel\RollbarServiceProvider'];
     }
 
     public function testBinding()
     {
-        $client = $this->app->make('RollbarNotifier');
-        $this->assertInstanceOf('RollbarNotifier', $client);
+        $client = $this->app->make('Rollbar\RollbarLogger');
+        $this->assertInstanceOf('Rollbar\RollbarLogger', $client);
 
-        $handler = $this->app->make('Jenssegers\Rollbar\RollbarLogHandler');
-        $this->assertInstanceOf('Jenssegers\Rollbar\RollbarLogHandler', $handler);
+        $handler = $this->app->make('Rollbar\Laravel\RollbarLogHandler');
+        $this->assertInstanceOf('Rollbar\Laravel\RollbarLogHandler', $handler);
     }
 
     public function testIsSingleton()
     {
-        $handler1 = $this->app->make('Jenssegers\Rollbar\RollbarLogHandler');
-        $handler2 = $this->app->make('Jenssegers\Rollbar\RollbarLogHandler');
+        $handler1 = $this->app->make('Rollbar\Laravel\RollbarLogHandler');
+        $handler2 = $this->app->make('Rollbar\Laravel\RollbarLogHandler');
         $this->assertEquals(spl_object_hash($handler1), spl_object_hash($handler2));
     }
 
     public function testFacade()
     {
-        $client = Rollbar::$instance;
-        $this->assertInstanceOf('RollbarNotifier', $client);
+        $client = RollbarFacade::getFacadeRoot();
+        $this->assertInstanceOf('Rollbar\Laravel\RollbarLogHandler', $client);
     }
 
     public function testPassConfiguration()
     {
-        $client = $this->app->make('RollbarNotifier');
-        $this->assertEquals($this->access_token, $client->access_token);
+        $client = $this->app->make('Rollbar\RollbarLogger');
+        $config = $client->extend([]);
+        $this->assertEquals($this->access_token, $config['access_token']);
     }
 
     public function testCustomConfiguration()
@@ -49,69 +52,72 @@ class RollbarTest extends Orchestra\Testbench\TestCase
         $this->app->config->set('services.rollbar.included_errno', E_ERROR);
         $this->app->config->set('services.rollbar.environment', 'staging');
 
-        $client = $this->app->make('RollbarNotifier');
-        $this->assertEquals('staging', $client->environment);
-        $this->assertEquals('/tmp', $client->root);
-        $this->assertEquals(E_ERROR, $client->included_errno);
+        $client = $this->app->make('Rollbar\RollbarLogger');
+        $config = $client->extend([]);
+        
+        $this->assertEquals('staging', $config['environment']);
+        $this->assertEquals('/tmp', $config['root']);
+        $this->assertEquals(E_ERROR, $config['included_errno']);
     }
 
     public function testAutomaticContext()
     {
         $this->app->session->put('foo', 'bar');
-
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->once()->with('Test log message', 'info', []);
-
-        $handlerMock = Mockery::mock('Jenssegers\Rollbar\RollbarLogHandler', [$clientMock, $this->app]);
+        
+        $logger = $this->app->make('Rollbar\RollbarLogger');
+        
+        $handlerMock = \Mockery::mock('Rollbar\Laravel\RollbarLogHandler', [$logger, $this->app]);
         $handlerMock->shouldReceive('log')->passthru();
-        $this->app['Jenssegers\Rollbar\RollbarLogHandler'] = $handlerMock;
-
-        $handler = $this->app->make('Jenssegers\Rollbar\RollbarLogHandler');
-        $handler->log('info', 'Test log message');
+        $this->app['Rollbar\Laravel\RollbarLogHandler'] = $handlerMock;
+        
+        $handlerMock->log('info', 'Test log message');
+        
+        $config = $logger->extend([]);
 
         $this->assertEquals([
             'session' => ['foo' => 'bar'],
             'id'      => $this->app->session->getId(),
-        ], $clientMock->person);
+        ], $config['person']);
     }
 
     public function testMergedContext()
     {
         $this->app->session->put('foo', 'bar');
-
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->once()->with('Test log message', 'info', [
-            'tags' => ['one' => 'two'],
-        ]);
-
-        $handlerMock = Mockery::mock('Jenssegers\Rollbar\RollbarLogHandler', [$clientMock, $this->app]);
+        
+        $logger = $this->app->make('Rollbar\RollbarLogger');
+        
+        $handlerMock = \Mockery::mock('Rollbar\Laravel\RollbarLogHandler', [$logger, $this->app]);
         $handlerMock->shouldReceive('log')->passthru();
-        $this->app['Jenssegers\Rollbar\RollbarLogHandler'] = $handlerMock;
-
-        $handler = $this->app->make('Jenssegers\Rollbar\RollbarLogHandler');
-        $handler->log('info', 'Test log message', [
+        $this->app['Rollbar\Laravel\RollbarLogHandler'] = $handlerMock;
+        
+        $handlerMock->log('info', 'Test log message', [
             'tags'   => ['one' => 'two'],
-            'person' => ['id'  => 1337, 'email' => 'john@doe.com'],
+            'person' => ['id'  => "1337", 'email' => 'john@doe.com'],
         ]);
+        
+        $config = $logger->extend([]);
 
         $this->assertEquals([
             'session' => ['foo' => 'bar'],
-            'id'      => 1337,
+            'id'      => "1337",
             'email'   => 'john@doe.com',
-        ], $clientMock->person);
+        ], $config['person']);
     }
 
     public function testLogListener()
     {
-        $exception = new Exception('Testing error handler');
+        $exception = new \Exception('Testing error handler');
 
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->times(2);
-        $clientMock->shouldReceive('report_exception')->times(1)->with($exception, null, ['foo' => 'bar']);
+        $clientMock = \Mockery::mock('Rollbar\RollbarLogger');
+        
+        $clientMock->shouldReceive('log')->times(2);
+        $clientMock->shouldReceive('log')->times(1)->with('error', $exception, ['foo' => 'bar']);
 
-        $handlerMock = Mockery::mock('Jenssegers\Rollbar\RollbarLogHandler', [$clientMock, $this->app]);
+        $handlerMock = \Mockery::mock('Rollbar\Laravel\RollbarLogHandler', [$clientMock, $this->app]);
+        
         $handlerMock->shouldReceive('log')->passthru();
-        $this->app['Jenssegers\Rollbar\RollbarLogHandler'] = $handlerMock;
+        
+        $this->app['Rollbar\Laravel\RollbarLogHandler'] = $handlerMock;
 
         $this->app->log->info('hello');
         $this->app->log->error('oops');
@@ -122,9 +128,9 @@ class RollbarTest extends Orchestra\Testbench\TestCase
     {
         $this->app->config->set('services.rollbar.level', 'critical');
 
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->times(3);
-        $this->app['RollbarNotifier'] = $clientMock;
+        $clientMock = \Mockery::mock('Rollbar\RollbarLogger');
+        $clientMock->shouldReceive('log')->times(3);
+        $this->app['Rollbar\RollbarLogger'] = $clientMock;
 
         $this->app->log->debug('hello');
         $this->app->log->info('hello');
@@ -140,9 +146,9 @@ class RollbarTest extends Orchestra\Testbench\TestCase
     {
         $this->app->config->set('services.rollbar.level', 'debug');
 
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->times(8);
-        $this->app['RollbarNotifier'] = $clientMock;
+        $clientMock = \Mockery::mock('Rollbar\RollbarLogger');
+        $clientMock->shouldReceive('log')->times(8);
+        $this->app['Rollbar\RollbarLogger'] = $clientMock;
 
         $this->app->log->debug('hello');
         $this->app->log->info('hello');
@@ -158,9 +164,9 @@ class RollbarTest extends Orchestra\Testbench\TestCase
     {
         $this->app->config->set('services.rollbar.level', 'none');
 
-        $clientMock = Mockery::mock('RollbarNotifier');
-        $clientMock->shouldReceive('report_message')->times(0);
-        $this->app['RollbarNotifier'] = $clientMock;
+        $clientMock = \Mockery::mock('Rollbar\RollbarLogger');
+        $clientMock->shouldReceive('log')->times(0);
+        $this->app['Rollbar\RollbarLogger'] = $clientMock;
 
         $this->app->log->debug('hello');
         $this->app->log->info('hello');

src/Facades/Rollbar.php
@@ -1,4 +1,4 @@
-<?php namespace Jenssegers\Rollbar\Facades;
+<?php namespace Rollbar\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
@@ -7,10 +7,10 @@ class Rollbar extends Facade
     /**
      * Get a schema builder instance for the default connection.
      *
-     * @return \Jenssegers\Rollbar\RollbarLogHandler
+     * @return \Rollbar\Laravel\RollbarLogHandler
      */
     protected static function getFacadeAccessor()
     {
-        return 'Jenssegers\Rollbar\RollbarLogHandler';
+        return 'Rollbar\Laravel\RollbarLogHandler';
     }
 }

src/RollbarServiceProvider.php
@@ -1,9 +1,9 @@
-<?php namespace Jenssegers\Rollbar;
+<?php namespace Rollbar\Laravel;
 
 use Illuminate\Support\ServiceProvider;
 use InvalidArgumentException;
-use Rollbar;
-use RollbarNotifier;
+use Rollbar\Rollbar;
+use Rollbar\Laravel\RollbarLogHandler;
 
 class RollbarServiceProvider extends ServiceProvider
 {
@@ -36,7 +36,7 @@ class RollbarServiceProvider extends ServiceProvider
                 $context = $args[2];
             }
 
-            $app['Jenssegers\Rollbar\RollbarLogHandler']->log($level, $message, $context);
+            $app['Rollbar\Laravel\RollbarLogHandler']->log($level, $message, $context);
         });
     }
 
@@ -52,46 +52,31 @@ class RollbarServiceProvider extends ServiceProvider
 
         $app = $this->app;
 
-        $this->app->singleton('RollbarNotifier', function ($app) {
-            // Default configuration.
+        $this->app->singleton('Rollbar\RollbarLogger', function ($app) {
+            
             $defaults = [
                 'environment'  => $app->environment(),
                 'root'         => base_path(),
             ];
-
             $config = array_merge($defaults, $app['config']->get('services.rollbar', []));
-
             $config['access_token'] = getenv('ROLLBAR_TOKEN') ?: $app['config']->get('services.rollbar.access_token');
-
+            
             if (empty($config['access_token'])) {
                 throw new InvalidArgumentException('Rollbar access token not configured');
             }
-
-            Rollbar::$instance = $rollbar = new RollbarNotifier($config);
-
-            return $rollbar;
+            
+            \Rollbar\Rollbar::init($config);
+            
+            return Rollbar::logger();
         });
 
-        $this->app->singleton('Jenssegers\Rollbar\RollbarLogHandler', function ($app) {
+        $this->app->singleton('Rollbar\Laravel\RollbarLogHandler', function ($app) {
+            
             $level = getenv('ROLLBAR_LEVEL') ?: $app['config']->get('services.rollbar.level', 'debug');
 
-            return new RollbarLogHandler($app['RollbarNotifier'], $app, $level);
+            return new RollbarLogHandler($app['Rollbar\RollbarLogger'], $app, $level);
+            
         });
-
-        // Register the fatal error handler.
-        register_shutdown_function(function () use ($app) {
-            if (isset($app['RollbarNotifier'])) {
-                $app->make('RollbarNotifier');
-                Rollbar::report_fatal_error();
-            }
-        });
-
-        // If the Rollbar client was resolved, then there is a possibility that there
-        // are unsent error messages in the internal queue, so let's flush them.
-        register_shutdown_function(function () use ($app) {
-            if (isset($app['RollbarNotifier'])) {
-                $app['RollbarNotifier']->flush();
-            }
-        });
+        
     }
 }

commit ddb7084f71a186dce6f463a3f96ce38a216b6c4b

Fix person_fn not being called when session not empty
Currently, when the session is not empty, this package always sets a
"person" entry in the rollbar config, which results in the rollbar/rollbar
package ever caling the person_fn.

The rollbar/rollbar package only calls $config['person_fn'] when the
$config['person'] doesn't already exist, which as stated above, will
always be set when the session is not empty.

Revert back to the behaviour originally found in RollbarLogHandler in
42920e5 before it was refactored in 8988c22 which introduced this bug.
As 42920e5 used an older version of the rollbar/rollbar library, this
commit doesn't revert the changes back exactly, but is the equivalent
changes for the latest version of the rollbar/rollbar library API.

Add a regression test that currently fails on master branch but passes
with these canges to RollbarLogHandler.

src/RollbarLogHandler.php
@@ -87,19 +87,33 @@ class RollbarLogHandler extends AbstractLogger
     {
         // Add session data.
         if ($session = $this->app->session->all()) {
+            $config = $this->logger->extend([]);
+
+            if (empty($config['person']) or ! is_array($config['person'])) {
+                $person = [];
+            } else {
+                $person = $config['person'];
+            }
+
             // Merge person context.
             if (isset($context['person']) and is_array($context['person'])) {
-                $this->logger->configure(['person' => $context['person']]);
+                $person = $context['person'];
                 unset($context['person']);
+            } else {
+                if (isset($config['person_fn']) && is_callable($config['person_fn'])) {
+                    $data = @call_user_func($config['person_fn']);
+                    if (isset($data['id'])) {
+                        $person = call_user_func($config['person_fn']);
+                    }
+                }
             }
 
             // Add user session information.
-            $config = $this->logger->extend([]);
-            $person = isset($config['person']) ? $config['person'] : [];
-            
-            $person['session'] = isset($person['session']) ?
-                array_merge($session, $person['session']) :
+            if (isset($person['session'])) {
+                $person['session'] = array_merge($session, $person['session']);
+            } else {
                 $person['session'] = $session;
+            }
 
             // User session id as user id if not set.
             if (! isset($person['id'])) {

tests/RollbarTest.php
@@ -177,4 +177,27 @@ class RollbarTest extends \Orchestra\Testbench\TestCase
         $this->app->log->alert('hello');
         $this->app->log->emergency('hello');
     }
+
+    public function testPersonFunctionIsCalledWhenSessionContainsAtLeastOneItem()
+    {
+        $this->app->config->set('services.rollbar.person_fn', function () {
+            return [
+                'id' => '123',
+                'username' => 'joebloggs',
+            ];
+        });
+
+        $logger = $this->app->make('Rollbar\RollbarLogger');
+
+        $this->app->session->put('foo', 'bar');
+
+        $this->app->log->debug('hello');
+
+        $config = $logger->extend([]);
+
+        $person = $config['person'];
+
+        $this->assertEquals('123', $person['id']);
+        $this->assertEquals('joebloggs', $person['username']);
+    }
 }