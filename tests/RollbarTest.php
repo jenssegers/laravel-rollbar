2375b552da15fd481112679db85c6acf6f603ecf
Artur Moczulski <artur.moczulski@gmail.com>
2018-08-17 00:56:17 +0000

    55: WIP tests for the new Laravel 5.6  logging setup

--- tests/RollbarTest.php
+++ tests/RollbarTest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Rollbar\Laravel;
+namespace Rollbar\Laravel\Tests;
 
-use Rollbar\Laravel\Facades\Rollbar as RollbarFacade;
 use Rollbar\Laravel\RollbarServiceProvider;
-use Rollbar\Laravel\RollbarLogHandler;
+use Rollbar\Laravel\MonologHandler;
 use Rollbar\RollbarLogger;
+use Monolog\Logger;
 
 class RollbarTest extends \Orchestra\Testbench\TestCase
 {
@@ -27,21 +27,15 @@ class RollbarTest extends \Orchestra\Testbench\TestCase
         $client = $this->app->make(RollbarLogger::class);
         $this->assertInstanceOf(RollbarLogger::class, $client);
 
-        $handler = $this->app->make(RollbarLogHandler::class);
-        $this->assertInstanceOf(RollbarLogHandler::class, $handler);
+        $handler = $this->app->make(MonologHandler::class);
+        $this->assertInstanceOf(MonologHandler::class, $handler);
     }
 
     public function testIsSingleton()
     {
-        $handler1 = $this->app->make(RollbarLogHandler::class);
-        $handler2 = $this->app->make(RollbarLogHandler::class);
+        $handler1 = $this->app->make(MonologHandler::class);
+        $handler2 = $this->app->make(MonologHandler::class);
         $this->assertEquals(spl_object_hash($handler1), spl_object_hash($handler2));
-    }
-
-    public function testFacade()
-    {
-        $client = RollbarFacade::getFacadeRoot();
-        $this->assertInstanceOf(RollbarLogHandler::class, $client);
     }
 
     public function testPassConfiguration()
@@ -69,140 +63,180 @@ class RollbarTest extends \Orchestra\Testbench\TestCase
     {
         $this->app->session->put('foo', 'bar');
         
-        $logger = $this->app->make(RollbarLogger::class);
+        $logger = \Mockery::mock(RollbarLogger::class, [
+            'access_token' => $this->access_token,
+            'environment' => 'test'
+        ]);
+        $logger->shouldReceive('extend')->passthru();
+        $logger->shouldReceive('log')->with('info', 'Test log message', []);
         
-        $handlerMock = \Mockery::mock(RollbarLogHandler::class, [$logger, $this->app]);
-        $handlerMock->shouldReceive('log')->passthru();
-        $this->app[RollbarLogHandler::class] = $handlerMock;
+        $handler = new MonologHandler($logger, Logger::INFO);
+        $handler->setApp($this->app);
         
-        $handlerMock->log('info', 'Test log message');
-        
-        $config = $logger->extend([]);
-
-        $this->assertEquals([
-            'session' => ['foo' => 'bar'],
-            'id'      => $this->app->session->getId(),
-        ], $config['person']);
-    }
-
-    public function testMergedContext()
-    {
-        $this->app->session->put('foo', 'bar');
-        
-        $logger = $this->app->make(RollbarLogger::class);
-        
-        $handlerMock = \Mockery::mock(RollbarLogHandler::class, [$logger, $this->app]);
-        $handlerMock->shouldReceive('log')->passthru();
-        $this->app[RollbarLogHandler::class] = $handlerMock;
-        
-        $handlerMock->log('info', 'Test log message', [
-            'tags'   => ['one' => 'two'],
-            'person' => ['id'  => "1337", 'email' => 'john@doe.com'],
+        $handler->handle([
+            'level' => Logger::INFO,
+            'message' => 'Test log message',
+            'context' => [],
+            'extra' => [],
+            'level_name' => 'INFO',
+            'channel' => 'local',
+            'datetime' => new \DateTime(),
+            'formatted' => 'foo'
         ]);
         
-        $config = $logger->extend([]);
-
-        $this->assertEquals([
-            'session' => ['foo' => 'bar'],
-            'id'      => "1337",
-            'email'   => 'john@doe.com',
-        ], $config['person']);
-    }
-
-    public function testLogListener()
-    {
-        $exception = new \Exception('Testing error handler');
-
-        $clientMock = \Mockery::mock(RollbarLogger::class);
+//           ["message"]=>
+//   string(34) "some message to be sent to rollbar"
+//   ["context"]=>
+//   array(0) {
+//   }
+//   ["level"]=>
+//   int(200)
+//   ["level_name"]=>
+//   string(4) "INFO"
+//   ["channel"]=>
+//   string(5) "local"
+//   ["datetime"]=>
+//   object(DateTime)#203 (3) {
+//     ["date"]=>
+//     string(26) "2018-08-16 02:56:18.300807"
+//     ["timezone_type"]=>
+//     int(3)
+//     ["timezone"]=>
+//     string(3) "UTC"
+//   }
+//   ["extra"]=>
+//   array(0) {
+//   }
+//   ["formatted"]=>
+//   string(71) "[2018-08-16 02:56:18] local.INFO: some message to be sent to rollbar  
+// "
         
-        $clientMock->shouldReceive('log')->times(2);
-        $clientMock->shouldReceive('log')->times(1)->with('error', $exception, ['foo' => 'bar']);
+        // $config = $logger->extend([]);
 
-        $handlerMock = \Mockery::mock(RollbarLogHandler::class, [$clientMock, $this->app]);
+        // $this->assertEquals([
+        //     'session' => ['foo' => 'bar'],
+        //     'id'      => $this->app->session->getId(),
+        // ], $config['person']);
+    }
+
+    // public function testMergedContext()
+    // {
+    //     $this->app->session->put('foo', 'bar');
         
-        $handlerMock->shouldReceive('log')->passthru();
+    //     $logger = $this->app->make(RollbarLogger::class);
         
-        $this->app[RollbarLogHandler::class] = $handlerMock;
+    //     $handlerMock = \Mockery::mock(RollbarLogHandler::class, [$logger, $this->app]);
+    //     $handlerMock->shouldReceive('log')->passthru();
+    //     $this->app[RollbarLogHandler::class] = $handlerMock;
+        
+    //     $handlerMock->log('info', 'Test log message', [
+    //         'tags'   => ['one' => 'two'],
+    //         'person' => ['id'  => "1337", 'email' => 'john@doe.com'],
+    //     ]);
+        
+    //     $config = $logger->extend([]);
 
-        $this->app->log->info('hello');
-        $this->app->log->error('oops');
-        $this->app->log->error($exception, ['foo' => 'bar']);
-    }
+    //     $this->assertEquals([
+    //         'session' => ['foo' => 'bar'],
+    //         'id'      => "1337",
+    //         'email'   => 'john@doe.com',
+    //     ], $config['person']);
+    // }
 
-    public function testErrorLevels1()
-    {
-        $this->app->config->set('logging.channels.rollbar.level', 'critical');
+    // public function testLogListener()
+    // {
+    //     $exception = new \Exception('Testing error handler');
 
-        $clientMock = \Mockery::mock(RollbarLogger::class);
-        $clientMock->shouldReceive('log')->times(3);
-        $this->app[RollbarLogger::class] = $clientMock;
+    //     $clientMock = \Mockery::mock(RollbarLogger::class);
+        
+    //     $clientMock->shouldReceive('log')->times(2);
+    //     $clientMock->shouldReceive('log')->times(1)->with('error', $exception, ['foo' => 'bar']);
 
-        $this->app->log->debug('hello');
-        $this->app->log->info('hello');
-        $this->app->log->notice('hello');
-        $this->app->log->warning('hello');
-        $this->app->log->error('hello');
-        $this->app->log->critical('hello');
-        $this->app->log->alert('hello');
-        $this->app->log->emergency('hello');
-    }
+    //     $handlerMock = \Mockery::mock(RollbarLogHandler::class, [$clientMock, $this->app]);
+        
+    //     $handlerMock->shouldReceive('log')->passthru();
+        
+    //     $this->app[RollbarLogHandler::class] = $handlerMock;
 
-    public function testErrorLevels2()
-    {
-        $this->app->config->set('logging.channels.rollbar.level', 'debug');
+    //     $this->app->log->info('hello');
+    //     $this->app->log->error('oops');
+    //     $this->app->log->error($exception, ['foo' => 'bar']);
+    // }
 
-        $clientMock = \Mockery::mock(RollbarLogger::class);
-        $clientMock->shouldReceive('log')->times(8);
-        $this->app[RollbarLogger::class] = $clientMock;
+    // public function testErrorLevels1()
+    // {
+    //     $this->app->config->set('logging.channels.rollbar.level', 'critical');
 
-        $this->app->log->debug('hello');
-        $this->app->log->info('hello');
-        $this->app->log->notice('hello');
-        $this->app->log->warning('hello');
-        $this->app->log->error('hello');
-        $this->app->log->critical('hello');
-        $this->app->log->alert('hello');
-        $this->app->log->emergency('hello');
-    }
+    //     $clientMock = \Mockery::mock(RollbarLogger::class);
+    //     $clientMock->shouldReceive('log')->times(3);
+    //     $this->app[RollbarLogger::class] = $clientMock;
 
-    public function testErrorLevels3()
-    {
-        $this->app->config->set('logging.channels.rollbar.level', 'none');
+    //     $this->app->log->debug('hello');
+    //     $this->app->log->info('hello');
+    //     $this->app->log->notice('hello');
+    //     $this->app->log->warning('hello');
+    //     $this->app->log->error('hello');
+    //     $this->app->log->critical('hello');
+    //     $this->app->log->alert('hello');
+    //     $this->app->log->emergency('hello');
+    // }
 
-        $clientMock = \Mockery::mock(RollbarLogger::class);
-        $clientMock->shouldReceive('log')->times(0);
-        $this->app[RollbarLogger::class] = $clientMock;
+    // public function testErrorLevels2()
+    // {
+    //     $this->app->config->set('logging.channels.rollbar.level', 'debug');
 
-        $this->app->log->debug('hello');
-        $this->app->log->info('hello');
-        $this->app->log->notice('hello');
-        $this->app->log->warning('hello');
-        $this->app->log->error('hello');
-        $this->app->log->critical('hello');
-        $this->app->log->alert('hello');
-        $this->app->log->emergency('hello');
-    }
+    //     $clientMock = \Mockery::mock(RollbarLogger::class);
+    //     $clientMock->shouldReceive('log')->times(8);
+    //     $this->app[RollbarLogger::class] = $clientMock;
 
-    public function testPersonFunctionIsCalledWhenSessionContainsAtLeastOneItem()
-    {
-        $this->app->config->set('logging.channels.rollbar.person_fn', function () {
-            return [
-                'id' => '123',
-                'username' => 'joebloggs',
-            ];
-        });
+    //     $this->app->log->debug('hello');
+    //     $this->app->log->info('hello');
+    //     $this->app->log->notice('hello');
+    //     $this->app->log->warning('hello');
+    //     $this->app->log->error('hello');
+    //     $this->app->log->critical('hello');
+    //     $this->app->log->alert('hello');
+    //     $this->app->log->emergency('hello');
+    // }
 
-        $logger = $this->app->make(RollbarLogger::class);
+    // public function testErrorLevels3()
+    // {
+    //     $this->app->config->set('logging.channels.rollbar.level', 'none');
 
-        $this->app->session->put('foo', 'bar');
+    //     $clientMock = \Mockery::mock(RollbarLogger::class);
+    //     $clientMock->shouldReceive('log')->times(0);
+    //     $this->app[RollbarLogger::class] = $clientMock;
 
-        $this->app->log->debug('hello');
+    //     $this->app->log->debug('hello');
+    //     $this->app->log->info('hello');
+    //     $this->app->log->notice('hello');
+    //     $this->app->log->warning('hello');
+    //     $this->app->log->error('hello');
+    //     $this->app->log->critical('hello');
+    //     $this->app->log->alert('hello');
+    //     $this->app->log->emergency('hello');
+    // }
 
-        $config = $logger->extend([]);
+    // public function testPersonFunctionIsCalledWhenSessionContainsAtLeastOneItem()
+    // {
+    //     $this->app->config->set('logging.channels.rollbar.person_fn', function () {
+    //         return [
+    //             'id' => '123',
+    //             'username' => 'joebloggs',
+    //         ];
+    //     });
 
-        $person = $config['person'];
+    //     $logger = $this->app->make(RollbarLogger::class);
 
-        $this->assertEquals('123', $person['id']);
-        $this->assertEquals('joebloggs', $person['username']);
-    }
+    //     $this->app->session->put('foo', 'bar');
+
+    //     $this->app->log->debug('hello');
+
+    //     $config = $logger->extend([]);
+
+    //     $person = $config['person'];
+
+    //     $this->assertEquals('123', $person['id']);
+    //     $this->assertEquals('joebloggs', $person['username']);
+    // }
 }
